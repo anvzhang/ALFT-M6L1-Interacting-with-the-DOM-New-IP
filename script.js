972acd45ab9ebce07e19a79b087bb63bed5bf41a
anvzhang <165081556+anvzhang@users.noreply.github.com>
2024-05-14 20:44:34 +0000

    done??

--- script.js
+++ script.js
@@ -46,3 +46,48 @@ eightElement.addEventListener("click", function() {
   let oneElement = document.querySelector(".one");
   oneElement.style.display = "none";
 });
+
+// Solution starts here
+
+let solutionElement = document.createElement("h5");
+solutionElement.innerHTML = "The solution is 4,2,6,7,3,5,1.";
+
+document.querySelector("h4:last-of-type").insertAdjacentElement("afterend", solutionElement);
+
+document.querySelectorAll("button").forEach(button => button.style.fontSize = "x-large");
+
+document.querySelector(".two").style.backgroundColor = "red";
+
+document.querySelector(".three").style.backgroundColor = "yellow";
+
+document.querySelector(".four").style.backgroundColor = "orange";
+
+document.querySelector(".five").style.backgroundColor = "purple";
+
+document.querySelector(".six").style.backgroundColor = "gray";
+
+document.querySelector(".seven").style.backgroundColor = "pink";
+
+document.querySelector(".eight").style.backgroundColor = "brown";
+
+
+document.querySelector("h3").style.textAlign = "center";
+
+document.querySelectorAll("h4").forEach(h4 => h4.style.textAlign = "center");
+
+document.querySelector("h5").style.textAlign = "center";
+
+
+document.querySelectorAll("button").forEach(button => button.addEventListener("click", function() {
+  document.body.style.backgroundColor = window.getComputedStyle(button).backgroundColor;
+}))
+
+let refreshButton = document.createElement("button")
+refreshButton.innerHTML = "refresh";
+
+document.body.append(refreshButton);
+
+refreshButton.addEventListener("click", function() {
+  document.querySelectorAll("button").forEach(button => button.style.removeProperty("display"));
+  document.body.style.removeProperty("background-color");
+});
